744a6c9c13fae31caab904b9c9572279611020d7
Candice Smith MacKenzie <Mahony8@aol.com>
2021-10-23 16:27:37 -0400

    commit tree fd61b8b4aab3f65c8b2c1a8d6cb57a4969730486

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers() {
+    let newArray = drivers.slice(0, 2);
+        return newArray;
+}    
+function returnLastTwoDrivers() {
+    let lastTwo = drivers.slice(2, 4);
+        return lastTwo;
+}
+const selectingDrivers = [`returnFirstTwoDrivers()`, `returnLastTwoDrivers()`];
+   // selectingDrivers.forEach(function )
+
+
+function createFareMultiplier(fare) {
+    return (fare * 2 );
+}
+ function fareDoubler() 
+
+ 
+     
+
